305f83212a94d36f494df49515ed56eebef602cb
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-08-03 17:15:55 -0400

    add route to delete and update

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -1,12 +1,11 @@
 const express = require("express")
 const warehouses = express.Router()
-const { getAllWarehouses, getOneWarehouse, addNewWarehouse} = require("../queries/warehouse.js")
+const { getAllWarehouses, getOneWarehouse, addNewWarehouse, deleteWarehouse, updateWarehouse} = require("../queries/warehouse.js")
 
 
 //Route to get all warehouses
 warehouses.get("/", async (req, res) => {
     const listOfWarehouses = await getAllWarehouses() 
-    console.log(listOfWarehouses)
     if (listOfWarehouses[0]) {
         res.status(200).json(listOfWarehouses);
     } else {
@@ -25,10 +24,33 @@ warehouses.get("/:id", async (req, res) => {
     }
 })
 
-//Route to add a warehouse to database
+//Route to add a warehouse to table
 warehouses.post("/add", async (req, res) => {
     const newWarehouse = await addNewWarehouse(req.body);
     res.status(200).json(newWarehouse)
+})
+
+//Route to delete a warehouse from table
+warehouses.delete("/:id", async (req, res) => {
+    const { id } = req.params;
+    const deletedWarehouse = await deleteWarehouse(id);
+    if (deletedWarehouse.id) {
+        res.status(200).json(deletedWarehouse);
+    } else {
+        res.status(404).json({error: "can not find warehouse"})
+    }
+})
+
+//Route to update a warehouse on table
+warehouses.put("/:id", async (req, res) => {
+    const { id } = req.params;
+    try{
+        const updatedWarehouse = await updateWarehouse(id, req.body)
+        res.status(200).json(updatedWarehouse);
+    } catch (error) {
+        res.status(400).json("Warehouse not found")
+    }
+    
 })
 
 module.exports = warehouses;
--- queries/warehouse.js
+++ queries/warehouse.js
@@ -13,7 +13,7 @@ const getAllWarehouses = async () => {
 // query to get a single warehouse
 const getOneWarehouse = async (id) => {
     try {
-        const currentWarehouse = await db.one("SELECT * FROM warehouses WHERE id=$1", id)
+        const currentWarehouse = await db.oneOrNone("SELECT * FROM warehouses WHERE id=$1", id)
         return currentWarehouse;
     } catch (error) {
         return error;
@@ -23,10 +23,34 @@ const getOneWarehouse = async (id) => {
 // query to add a warehouse to database
 const addNewWarehouse = async ( warehouse ) => {
     try {
-        const newWarehouse = await db.one("INSERT INTO warehouses (nickname, date_created, owner, pallet_capacity, location) VALUES ($1, $2, $3, $4, $5) RETURNING *",[warehouse.nickname, warehouse.date_created, warehouse.owner, warehouse.pallet_capacity, warehouse.location])
+        const newWarehouse = await db.oneOrNone("INSERT INTO warehouses (nickname, date_created, owner, pallet_capacity, location) VALUES ($1, $2, $3, $4, $5) RETURNING *",[warehouse.nickname, warehouse.date_created, warehouse.owner, warehouse.pallet_capacity, warehouse.location])
         return newWarehouse
     } catch (error) {
         return (error)
     }
 }
-module.exports = { getAllWarehouses, getOneWarehouse, addNewWarehouse }
+
+// query to delete a warehouse from table
+const deleteWarehouse = async ( id ) => {
+    try {
+        const warehouseToBeDeleted = await db.oneOrNone ("DELETE FROM warehouses WHERE id=$1 RETURNING *", id);
+        return warehouseToBeDeleted
+    } catch (error) {
+        return error
+    }
+}
+
+// query to update a warehouse in the table
+const updateWarehouse = async ( id, warehouse) => {
+    try {
+        const wareHouseToBeUpdated = await db.oneOrNone ("UPDATE warehouses SET nickname=$1, date_created=$2, owner=$3, pallet_capacity=$4, location=$5 WHERE id=$6 RETURNING *",
+            [warehouse.nickname, warehouse.date_created, warehouse.owner, warehouse.pallet_capacity, warehouse.location, id]
+        )
+        return wareHouseToBeUpdated
+    } catch (error) {
+        return error
+    }
+}
+
+
+module.exports = { getAllWarehouses, getOneWarehouse, addNewWarehouse, deleteWarehouse, updateWarehouse }
